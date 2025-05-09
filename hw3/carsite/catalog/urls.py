5438b2584206b437097e9b462c3dea970a69e69e
df530 <df530@yandex-team.ru>
2025-05-09 13:29:41 +0300

    Hw3, MVP

---
+++ hw3/carsite/catalog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('cars/', views.car_list, name='car_list'),
+    path('cars/<int:car_id>/', views.car_detail, name='car_detail'),
+]
